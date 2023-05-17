83cd562a95350746b3353c99eee874f7467515a2
barnaor97 <barnao@post.bgu.ac.il>
2023-05-17 11:54:45 +0300

    part b
    changes to part b

--- part_b/static/createNewOrder.js
+++ part_b/static/createNewOrder.js
@@ -5,12 +5,8 @@ let number = document.querySelector('#number')
 const start = document.querySelector('#start')
 const end = document.querySelector('#end')
 let kids_num = document.querySelector('#kids-number')
-const now = new Date();
-const startDateTime = new Date(start.value);
-const endDateTime = new Date (end.value)
 const msg = document.querySelector('.msg')
 
-// const orders_list = document.querySelector('.orders_list')
 const on_submit = (e) => {
     e.preventDefault()  /*so the user won't click on submit by accident or of the form is not filled correctly*/
 
@@ -26,30 +22,53 @@ const on_submit = (e) => {
         msg.innerHTML = 'Please enter a valid address number'
         msg.classList.add('error')
     }
-    else if ((startDateTime.getTime() >= now.getTime() || endDateTime.getTime() <= startDateTime.getTime() || start.value === '' || end.value === '')) {
-        msg.innerHTML = 'Please enter a valid start and end time'
+    else if(start.value === '' || end.value === ''){//if the dates are empty
+        msg.innerHTML = 'Please enter start and end time'
         msg.classList.add('error')
     }
+
+    else if (!validateDate(start.value, end.value)) {
+        msg.innerHTML = 'Please enter valids dates';
+        msg.classList.add('error');
+    }
+
+
     else if(kids_num.value <= 0){
         msg.innerHTML = 'Please enter a valid kids number'
         msg.classList.add('error')
     }
     else{
-        // const li = document.createElement('li')
-        // li.appendChild(document.createTextNode(`${street.value}: ${city.value}: ${number.value}: ${start.value}: ${endDateTime.value}: ${kids_num.value}`))
-        msg.classList.remove('error')
-        msg.innerHTML = 'Form submitted successfully';
-        // orders_list.appendChild(li)
-
+        //empty the boxes
         street.value = ''
         city.value = ''
         number.value = ''
         start.value = ''
-        endDateTime.value = ''
         end.value = ''
         kids_num.value = ''
+        msg.classList.remove('error')
+        msg.innerHTML = 'Form submitted successfully';
     }
 }
 form.addEventListener('submit', on_submit)
 
 
+function validateDate(start, end) {
+       const now = new Date();
+        const s = new Date(start);
+        const e = new Date(end);
+    if (now.getDate() > s.getDate()) {
+        return false;
+    }
+    else if (now.getDate() === s.getDate() && now.getTime() >= s.getTime()) {
+        return false;
+    }
+    else if (s.getDate() > e.getDate()){
+        return false;
+    }
+    else if (s.getDate() === e.getDate() && s.getTime() >= e.getTime()){
+        return false;
+    }
+    else {
+        return true;
+    }
+}
